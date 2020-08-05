bf095ae5896dc638a8c7718a4cb97ea3a68cf705
hiroki <hiroki.2585@gmail.com>
2020-08-05 23:09:36 +0900

    Initial Commit

---
+++ ArchitectureSample-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureSample-iOS
+//
+//  Created by Hiroki on 2020/08/05.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
