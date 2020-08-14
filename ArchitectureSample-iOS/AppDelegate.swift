abfb4fc9448e5039d14111152d3f9aee1c1ceb07
hiroki <hiroki.2585@gmail.com>
2020-08-14 21:57:22 +0900

    リポジトリ検索画面の簡易UI実装

--- ArchitectureSample-iOS/AppDelegate.swift
+++ ArchitectureSample-iOS/AppDelegate.swift
@@ -15,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let vc = GitHubSearchRepositoryListViewController()
+        let ui = GitHubSearchRepositoryUIImpl()
+        ui.viewController = vc
+        let presenter = GitHubSearchRepositoryPresenterImpl()
+        vc.inject(ui: ui, presenter: presenter)
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: vc)
+        window?.makeKeyAndVisible()
+        
+        
         return true
     }
 }
