400e165a774bc72810fa5bcb307700a02b9416b9
hiroki <hiroki.2585@gmail.com>
2020-08-15 16:53:09 +0900

    DataLayerのファイル追加

---
+++ ArchitectureSample-iOS/Data/DataStore/GitHubSearchRepositoryListDataStore.swift
@@ -0,0 +1,9 @@
+//
+//  GitHubSearchRepositoryListDataStore.swift
+//  ArchitectureSample-iOS
+//
+//  Created by Hiroki on 2020/08/15.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
