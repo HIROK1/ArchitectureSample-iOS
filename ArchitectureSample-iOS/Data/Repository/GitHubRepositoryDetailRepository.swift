91ef4ce301fa609b818ef54379abb67ab244007e
hiroki <hiroki.2585@gmail.com>
2020-09-22 17:12:55 +0900

    追加: RepositoryDetailRepositoryファイル

---
+++ ArchitectureSample-iOS/Data/Repository/GitHubRepositoryDetailRepository.swift
@@ -0,0 +1,28 @@
+//
+//  GitHubRepositoryDetailRepository.swift
+//  ArchitectureSample-iOS
+//
+//  Created by Hiroki on 2020/09/20.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+protocol GitHubRepositoryDetailRepository: AnyObject {
+    func fetchReadme(owner: String, repositoryName: String, completion: @escaping (Data?, URLResponse?, Error?) -> Void)
+}
+
+final class GitHubRepositoryDetailRepositoryImpl: GitHubRepositoryDetailRepository {
+    
+    private let dataStore: GitHubRepositoryDetailDataStore
+    
+    init(dataStore: GitHubRepositoryDetailDataStore) {
+        self.dataStore = dataStore
+    }
+    
+    func fetchReadme(owner: String, repositoryName: String, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        dataStore.fetchReadme(owner: owner, repositoryName: repositoryName) { data, response, error in
+            completion(data, response, error)
+        }
+    }
+}
