e21b937f272a47460c9a82c6183a55f33bc036c6
hiroki <hiroki.2585@gmail.com>
2020-09-22 17:10:25 +0900

    追加: ReadmeModelファイル

---
+++ ArchitectureSample-iOS/Domain/Model/GitHubRepositoryReadmeModel.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubRepositoryReadmeModel.swift
+//  ArchitectureSample-iOS
+//
+//  Created by Hiroki on 2020/09/20.
+//  Copyright © 2020 Hiroki. All rights reserved.
+//
+
+import Foundation
+
+struct GitHubRepositoryReadmeModel {
+    let readmeString: String
+}
